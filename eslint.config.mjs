74766b337e49cde9bd157fac78cdfd6e9664b859
Max DeLiso <me@maxdeliso.name>
2026-02-24 14:08:16 -0500

    Add ESLint with Prettier integration

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+import prettier from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
